6bebf79a490788db89dca7714bf25980e4de37c6
shubendu <biswasshubendu4@gmail.com>
2024-01-11 00:37:14 +0530

    updated basic Product and Collection APIs

--- store/serializers.py
+++ store/serializers.py
@@ -3,19 +3,19 @@ from store.models import Product, Collection
 from decimal import Decimal
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title','products_count']
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source ="unit_price")
+    products_count = serializers.IntegerField()
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','title', 'slug', 'inventory', 'unit_price','description','price_with_tax' ,'collection']
+   
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.HyperlinkedRelatedField(
-        queryset = Collection.objects.all(),
-        view_name='collection-detail'
-    )
 
     def calculate_tax(self,product: Product):
         return product.unit_price * Decimal(1.1)
